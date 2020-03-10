81675254af4a36e5e31c2d4571e3e403a511d0fd
Syd520 <2386121584@qq.com>
2020-03-10 17:06:33 +0800

    Vue

--- wanghejun/src/main/java/com/project/controller/StudentController.java
+++ wanghejun/src/main/java/com/project/controller/StudentController.java
@@ -1,14 +1,13 @@
 package com.project.controller;
 
 import com.project.bean.StudentBean;
+import com.project.bean.UserBean;
+import com.project.bean.UserInfoBean;
 import com.project.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @RestController
 public class StudentController {
@@ -20,5 +19,11 @@ public class StudentController {
     public StudentBean findName(@RequestParam("name") String name){
         System.out.println(name);
         return service.findName(name);
+    }
+
+    @RequestMapping("addUser.test")
+    public void addUser(UserBean user){
+        service.addUser(user);
+        service.addUserInfo(new UserInfoBean());
     }
 }
--- wanghejun/src/main/java/com/project/service/IStudentService.java
+++ wanghejun/src/main/java/com/project/service/IStudentService.java
@@ -1,6 +1,8 @@
 package com.project.service;
 
 import com.project.bean.StudentBean;
+import com.project.bean.UserBean;
+import com.project.bean.UserInfoBean;
 
 import java.util.List;
 
@@ -9,5 +11,9 @@ public interface IStudentService {
     List<StudentBean> findAll();
 
     StudentBean findName(String name);
+
+    void addUser(UserBean user);
+
+    void addUserInfo(UserInfoBean userInfo);
 
 }
